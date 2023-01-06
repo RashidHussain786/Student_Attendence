d41470c7855a2e0f37b2b4512df9d108fc02081c
Rashid <rashiddss3@gmail.com>
2023-01-06 17:27:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+ // State variables to store the list of students, input values, and the current time
+ const [students, setStudents] = useState([]);
+ const [rollNumber, setRollNumber] = useState('');
+ const [name, setName] = useState('');
+ const [currentTime, setCurrentTime] = useState('');
+
+ // Function to add a new student to the list
+ const addStudent = () => {
+   const newStudent = { rollNumber, name, checkInTime: currentTime };
+   setStudents([...students, newStudent]);
+ }
+
+ // Function to remove a student from the list
+ const removeStudent = (rollNumber) => {
+   const updatedStudents = students.filter(student => student.rollNumber !== rollNumber);
+   setStudents(updatedStudents);
+ }
+
+ // useEffect hook to update the current time every second
+ useEffect(() => {
+   const interval = setInterval(() => {
+     const currentDate = new Date();
+     setCurrentTime(currentDate.toLocaleTimeString());
+   }, 1000);
+   return () => clearInterval(interval);
+ }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='maincontainer'>
+      <div className='form-container'>
+      <h1>Student Attendance</h1>
+    <div className='input'>
+      Roll Number <br/>
+      <input type='rollnumber' value={rollNumber} onChange={e => setRollNumber(e.target.value)} />
     </div>
+    <div className='input' >
+      Student Name <br/>
+       <input type='name' value={name} onChange={e => setName(e.target.value)} />
+    </div>
+    <button type='button' onClick={addStudent}>Check In</button>
+      </div>
+    <div className='container'>
+    <h2>students present in the school today ({students.length})</h2>
+    <table>
+    <tr>
+    <th>Roll Number</th>
+    <th>Student Name</th>
+    <th>Check In Time</th>
+    <th>Check Out</th>
+  </tr>
+  <tbody>
+    {students.map(student => (
+      <tr key={student.rollNumber}>
+        <td>{student.rollNumber}</td>
+        <td>{student.name}</td>
+        <td>{student.checkInTime}</td>
+        <td>
+          <button onClick={() => removeStudent(student.rollNumber)}>Check Out</button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+    </table>
+    </div>
+  </div>
+  </>
   );
 }
 
